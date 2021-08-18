1b5d85edf15fdcfea7ea53d8917ed6331bbb7494
kienk93 <kienk93@gmail.com>
2021-08-18 10:11:09 +0700

    th- component interaction

---
+++ angular-process-bar/src/app/process-bar/process-bar.component.ts
@@ -0,0 +1,25 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-process-bar',
+  templateUrl: './process-bar.component.html',
+  styleUrls: ['./process-bar.component.scss']
+})
+export class ProcessBarComponent implements OnInit {
+  @Input() backgroundColor = '#D9D9D9';
+  @Input() progressColor = '#4CAF50';
+  @Input() progress = 0;
+  constructor() { }
+
+  ngOnInit() {
+    this.loading();
+  }
+  loading() {
+    const interval = setInterval(() => {
+      this.progress = +this.progress + 5;
+      if (this.progress === 100) {
+        clearInterval(interval);
+      }
+    }, 500);
+  }
+}
